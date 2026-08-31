ebdf778832276576eb10450ef7e9a5087d32bcb8
Caylee <kira07210@gmail.com>
2026-08-31 17:15:30 +0000

    m1hw1 movie final changes

---
+++ m1hw1_castillo.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M1HW
+// Caylee Castillo
+// 08/31/2026
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // This program will discuss the movie "Catch Me If You Can".
+    // The movie's title
+    string title = "Catch Me If You Can";
+    // The year the movie was released
+    int yearReleased = 2002;
+    // The movie's box office gross revenue
+    double boxOfficeGross = 352.1; // in millions
+
+    // print all the information about the movie
+    cout << "The movie \"" << title << "\" was released in " << yearReleased << endl;
+    cout << "and grossed $" << boxOfficeGross << " million at the box office." << endl;
+   
+    // Final line, a fun quote from the movie
+    cout << "Frank Abagnale Jr. : \"Dr. Harris, do you concur?\"" << endl;
+    cout << "Dr. Harris : \"Yes?\"" << endl;   
+    cout << "Frank Abagnale Jr. : \"Do you concur?\"" << endl;
+    cout << "Dr. Harris : \"Concur with what, sir?\"" << endl;
+    cout << "Dr. Harris : \"I blew it didn't I? Why didn't I concur?\"" << endl;
+   
+    return 0;
+
+}
+
+    
