edd0b60e45f4556a7531585855562698d45f5843
Caylee <kira07210@gmail.com>
2026-09-17 15:20:28 +0000

    final

--- m2t1_castillo/m2t1_castillo.cpp
+++ m2t1_castillo/m2t1_castillo.cpp
@@ -5,13 +5,15 @@
 // We're going to make the simplest possible
 // "checkout" machine.
 #include <iostream>
+#include <iomanip> // for two decimal places
 using namespace std;
 
 int main() {
 
   // Set up all variables
   string first_name, last_name, full_name; // holds customer name
-  string product = "video game"; // holds product name
+  string product = "video games"; // holds product name
+  int amount_purchased; // holds amount of product
   double cost_each = 59.99; // holds product cost
   double total_cost; // holds total cost
   
@@ -27,10 +29,19 @@ int main() {
 
 
     // Ask how much they'd like to purchase
-    
+    cout << "How many " << product << " would you like today? ";
+    cin >> amount_purchased;
 
     // Calculate total price
+    total_cost = amount_purchased * cost_each;
+
+    // Formatting: set all prices to decimal places
+    cout << setprecision(2) << fixed;
+    
     // Give the result
+    cout << "For " << amount_purchased << " " << product << endl;
+    cout << "That will be $" << total_cost << endl;
+    cout << "Thank you for shopping with us, " << endl;
 
 
     return 0;
