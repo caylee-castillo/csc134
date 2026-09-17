ff90e8b6325a0322984e2f46e66f5532855fa8f1
Caylee <kira07210@gmail.com>
2026-09-17 15:07:54 +0000

    m12t1 started

---
+++ m2t1_castillo/m2t1_castillo.cpp
@@ -0,0 +1,38 @@
+// CSC 134 
+// M2T1 - Product sales
+// Caylee Castillo
+// 9/17/2026
+// We're going to make the simplest possible
+// "checkout" machine.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Set up all variables
+  string first_name, last_name, full_name; // holds customer name
+  string product = "video game"; // holds product name
+  double cost_each = 59.99; // holds product cost
+  double total_cost; // holds total cost
+  
+
+    // Greet the customer
+    cout << "Welcome to our " << product << " store!" << endl;
+    cout << "What is your first name? ";
+    cin >> first_name;
+    cout << "What is your last name? ";
+    cin >> last_name;
+    full_name = first_name + " " + last_name;
+    cout << "Nice to meet you, " << full_name << endl;
+
+
+    // Ask how much they'd like to purchase
+    
+
+    // Calculate total price
+    // Give the result
+
+
+    return 0;
+
+}
